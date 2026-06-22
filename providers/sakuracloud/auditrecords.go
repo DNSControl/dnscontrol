57a38da4e47b5555c2068f61739aac2db5946707
Tom Limoncelli <tal@whatexit.org>
2026-06-22 19:43:05 -0400

    fix rejectif

--- providers/sakuracloud/auditrecords.go
+++ providers/sakuracloud/auditrecords.go
@@ -6,6 +6,8 @@ import (
 	"slices"
 	"strings"
 
+	dnsv2 "codeberg.org/miekg/dns"
+	dnsrdatav2 "codeberg.org/miekg/dns/rdata"
 	"github.com/DNSControl/dnscontrol/v4/models"
 	"github.com/DNSControl/dnscontrol/v4/pkg/rejectif"
 	dnsv1 "github.com/miekg/dns"
@@ -79,6 +81,9 @@ func hasLabelExample(domain string) error {
 //   - example
 //   - exampleN, where N is a numerical character
 func rejectifTargetHasExample(rc *models.RecordConfig) error {
+	if rc.TypeNum == dnsv2.TypeHTTPS || rc.TypeNum == dnsv2.TypeSVCB {
+		return hasLabelExample(rc.GetRDATA().(dnsrdatav2.SVCB).Target)
+	}
 	return hasLabelExample(rc.GetTargetField())
 }
 
