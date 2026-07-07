250f6d1607acb637ad9652136974215801f1c481
Tom Limoncelli <tal@whatexit.org>
2026-07-06 23:24:52 -0400

    Simplify t_*.go files

--- models/t_naptr.go
+++ models/t_naptr.go
@@ -1,53 +1,23 @@
 package models
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+	dnsv2 "codeberg.org/miekg/dns"
 )
 
 // SetTargetNAPTR sets the NAPTR fields.
+// Deprecated. Use models.NewRecordConfig() instead.
 func (rc *RecordConfig) SetTargetNAPTR(order uint16, preference uint16, flags string, service string, regexp string, target string) error {
-	if target == "" {
-		target = "."
-	}
-	rc.NaptrOrder = order
-	rc.NaptrPreference = preference
-	rc.NaptrFlags = flags
-	rc.NaptrService = service
-	rc.NaptrRegexp = regexp
-	if err := rc.SetTarget(target); err != nil {
-		return err
-	}
-
-	if rc.Type == "" {
-		rc.Type = "NAPTR"
-	}
-	if rc.Type != "NAPTR" {
-		panic("assertion failed: SetTargetNAPTR called when .Type is not NAPTR")
-	}
-
-	return nil
+	return legacySetTargetArgs(rc, dnsv2.TypeNAPTR, order, preference, flags, service, regexp, target)
 }
 
-// SetTargetNAPTRStrings is like SetTargetNAPTR but accepts strings.
-func (rc *RecordConfig) SetTargetNAPTRStrings(order, preference, flags string, service string, regexp string, target string) error {
-	i64order, err := strconv.ParseUint(order, 10, 16)
-	if err != nil {
-		return fmt.Errorf("NAPTR order does not fit in 16 bits: %w", err)
-	}
-	i64preference, err := strconv.ParseUint(preference, 10, 16)
-	if err != nil {
-		return fmt.Errorf("NAPTR preference does not fit in 16 bits: %w", err)
-	}
-	return rc.SetTargetNAPTR(uint16(i64order), uint16(i64preference), flags, service, regexp, target)
-}
+// // SetTargetNAPTRStrings is like SetTargetNAPTR but accepts strings.
+// Deprecated. Use models.NewRecordConfig() instead.
+// func (rc *RecordConfig) SetTargetNAPTRStrings(order, preference, flags string, service string, regexp string, target string) error {
+// 	return legacySetTargetArgs(rc, dnsv2.TypeNAPTR, order, preference, flags, service, regexp, target)
+// }
 
 // SetTargetNAPTRString is like SetTargetNAPTR but accepts one big string.
+// Deprecated. Use models.NewRecordConfigParse() instead.
 func (rc *RecordConfig) SetTargetNAPTRString(s string) error {
-	part := strings.Fields(s)
-	if len(part) != 6 {
-		return fmt.Errorf("NAPTR value does not contain 6 fields: (%#v)", s)
-	}
-	return rc.SetTargetNAPTRStrings(part[0], part[1], StripQuotes(part[2]), StripQuotes(part[3]), StripQuotes(part[4]), StripQuotes(part[5]))
+	return legacySetTargetParse(rc, dnsv2.TypeNAPTR, s)
 }
