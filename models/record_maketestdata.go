458b8a3231be40e04865810b0b1d4e1d0ed53f64
Tom Limoncelli <tal@whatexit.org>
2026-07-11 12:36:56 -0400

    CHORE: Add Must* equivs of RecordConfig factories

--- models/record_maketestdata.go
+++ models/record_maketestdata.go
@@ -7,6 +7,30 @@ import (
 	"github.com/DNSControl/dnscontrol/v4/pkg/mustbe"
 )
 
+// MustNewRecordConfig is like models.NewRecordConfig but panics if initialization fails.
+// It is intended for use in variable initializations in unit tests.
+// Before using this, consider using models.AddTestRC() first.
+func (dc *DomainConfig) MustNewRecordConfig(name string, ttl uint32, typeAny any, args ...any) *RecordConfig {
+	rc, err := NewRecordConfig(name, ttl, typeAny, args)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// MustNewRecordConfigParse is like NewRecordConfigParse but panics if initialization fails.
+// It is intended for use in variable initializations in unit tests.
+// Before using this, consider using models.AddTestRCParse() first.
+func (dc *DomainConfig) MustNewRecordConfigParse(name string, ttl uint32, typeAny any, data string) *RecordConfig {
+	rc, err := NewRecordConfigParse(name, ttl, typeAny, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// AddTestRC() is a convenience function that uses
+// models.NewRecordConfig() to create a models.RecordConfig and adds it to
+// a models.DomainConfig. It is for use in unit tests.
+// It panics on error.
 func (dc *DomainConfig) AddTestRC(t *testing.T, label string, ttl uint32, typeNum uint16, args ...any) *RecordConfig {
 	mustbe.ValidArgs(args)
 	rc, err := dc.NewRecordConfig(label, ttl, typeNum, args...)
@@ -18,8 +42,12 @@ func (dc *DomainConfig) AddTestRC(t *testing.T, label string, ttl uint32, typeNu
 	return rc
 }
 
+// AddTestRCParse() is a convenience function that uses
+// models.NewRecordConfigParse() to create a models.RecordConfig and adds it to
+// a models.DomainConfig. It is for use in unit tests.
+// It panics on error.
 func (dc *DomainConfig) AddTestRCParse(label string, ttl uint32, typeNum uint16, contents string) *RecordConfig {
-	rc, err := dc.NewRecordConfigParse(label, ttl, typeNum, contents)
+	rc := dc.NewRecordConfigParse(label, ttl, typeNum, contents)
 	if err != nil {
 		panic(fmt.Sprintf("dc.NewRecordConfigParse() returned %v", err))
 	}
