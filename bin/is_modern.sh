92e88531681f4a980836bd82709b4ed746c86541
Tom Limoncelli <tal@whatexit.org>
2026-07-17 10:14:00 -0400

    Add converstion doc

--- bin/is_modern.sh
+++ bin/is_modern.sh
@@ -5,12 +5,18 @@ if [[ $? -ne 0 ]] ; then
     exit 1
 fi
 
+echo '========== DomainConfig{'
+grep --color --include='*.go' -r -F 'DomainConfig{' *
+
 echo '========== RecordConfig{'
 grep --color --include='*.go' -r -F 'RecordConfig{' *
+
 echo '========== PopulateFromString{'
 grep --color --include='*.go' -r -F 'PopulateFromString' *
+
 echo '========== SetTarget'
 grep --color --include='*.go' -r -E 'GetTargetCombinedFunc\(|GetTargetCombined\(|GetTargetRFC1035Quoted\('
+
 echo '========== GetTarget'
 grep --color --include='*.go' -r -E 'SetTargetCAA\(|SetTargetCAAStrings\(|SetTargetCAAString\(|SetTargetDNSKEYString\(|SetTargetDSString\(|SetTargetLOCString\(|SetTargetMX\(|SetTargetMXString\(|SetTargetNAPTR\(|SetTargetNAPTRString\(|SetTargetSMIMEA\(|SetTargetSOA\(|SetTargetSRV\(|SetTargetSRVPriorityString\(|SetTargetSRVString\(|SetTargetSSHFP\(|SetTargetSSHFPStrings\(|SetTargetSSHFPString\(|SetTargetSVCBString\(|SetTargetTLSA\(|SetTargetTLSAString\('
 
