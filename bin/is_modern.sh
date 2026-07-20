4103322f1828d1e1726a5448b312e2d25882d3dc
Tom Limoncelli <tlimoncelli@coreweave.com>
2026-07-19 21:45:15 -0400

    wip!

--- bin/is_modern.sh
+++ bin/is_modern.sh
@@ -1,12 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
 cd "${1:-.}"
+if [[ $? -ne 0 ]] ; then
+    exit 1
+fi
 
-echo '========== RecordConfig{'
-grep --include='*.go' -r -F 'RecordConfig{' *
-echo '========== PopulateFromString{'
-grep --include='*.go' -r -F 'PopulateFromString' *
-echo '========== SetTarget'
-grep --include='*.go' -r -F 'SetTarget' *
-echo '========== GetTarget'
-grep --include='*.go' -r -F 'GetTarget' *
+echo '========== Step 1. DomainConfig{'
+grep --color --include='*.go' -r -F 'DomainConfig{' *
+
+echo '========== Step 2. RecordConfig{'
+grep --color --include='*.go' -r -F 'RecordConfig{' *
+
+echo '========== Step 3. PopulateFromString{'
+grep --color --include='*.go' -r -F 'PopulateFromString' *
+
+echo '========== Step 4. AddOrigin('
+grep --color --include='*.go' -r -F 'AddOrigin(' *
+
+echo '========== Step 5. TrimDomainName('
+grep --color --include='*.go' -r -F 'TrimDomainName(' *
+
+echo '========== Step 6. SetTarget'
+grep --color --include='*.go' -r -E 'GetTargetCombinedFunc\(|GetTargetCombined\(|GetTargetRFC1035Quoted\('
+
+echo '========== Step 7. GetTarget'
+grep --color --include='*.go' -r -E 'SetTargetCAA\(|SetTargetCAAStrings\(|SetTargetCAAString\(|SetTargetDNSKEYString\(|SetTargetDSString\(|SetTargetLOCString\(|SetTargetMX\(|SetTargetMXString\(|SetTargetNAPTR\(|SetTargetNAPTRString\(|SetTargetSMIMEA\(|SetTargetSOA\(|SetTargetSRV\(|SetTargetSRVPriorityString\(|SetTargetSRVString\(|SetTargetSSHFP\(|SetTargetSSHFPStrings\(|SetTargetSSHFPString\(|SetTargetSVCBString\(|SetTargetTLSA\(|SetTargetTLSAString\('
