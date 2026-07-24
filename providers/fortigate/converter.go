d83db049ba1f7074f863a0c17384acdf2a2243e0
Tom Limoncelli <tal@whatexit.org>
2026-07-24 09:44:32 -0400

    dc.LabelFromFQDNWithDot -> dc.LabelFromShort

--- providers/fortigate/converter.go
+++ providers/fortigate/converter.go
@@ -11,7 +11,7 @@ import (
 func nativeToRecord(dc *models.DomainConfig, n fgDNSRecord) (*models.RecordConfig, error) {
 	rtype := strings.ToUpper(n.Type)
 
-	label := dc.LabelFromFQDNWithDot(n.Hostname)
+	label := dc.LabelFromShort(n.Hostname)
 	ttl := n.TTL
 
 	// Type-specific fields
