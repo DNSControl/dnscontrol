9c08845276b9cb054feb22c2a77843851e52b213
Tom Limoncelli <tal@whatexit.org>
2026-07-28 09:11:17 -0400

    remove comments

--- providers/fortigate/converter.go
+++ providers/fortigate/converter.go
@@ -10,14 +10,6 @@ import (
 // nativeToRecord – convert an fgDNSRecord coming from FortiGate into a *models.RecordConfig that dnscontrol understands.
 func nativeToRecord(dc *models.DomainConfig, n fgDNSRecord) (*models.RecordConfig, error) {
 
-	//spew.Dump(n)
-	// TODO(tlim): Please uncomment the above line and run the full integration tests. Use the `-v` flag. I'd like the full output. Thanks!
-	// The output will be very large, but it will give me info I need to prevent future regressions.
-	// Run the integration tests with:
-	// 		cd integrationTest
-	// 		go test -timeout 1h -v -args -verbose -profile FORTIGATE
-	// Thanks!
-
 	rtype := strings.ToUpper(n.Type)
 
 	var label string
