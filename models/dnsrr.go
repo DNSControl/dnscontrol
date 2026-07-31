9b9e49d1d9b91b852c8629c3d2d026af7adec3f7
Tom Limoncelli <tal@whatexit.org>
2026-07-31 14:23:01 -0400

    REFACTOR: Remove Records.String()

--- models/dnsrr.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// methods that make RecordConfig meet the dns.RR interface.
-
-// String returns the text representation of the resource record.
-func (rc *RecordConfig) xString() string {
-	return rc.GetTargetCombined()
-}
