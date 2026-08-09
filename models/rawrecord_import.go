7721158793fd5c987b4ef82e33952d974174a65c
Tom Limoncelli <tal@whatexit.org>
2026-08-09 16:08:28 -0400

    fixup!

--- models/rawrecord_import.go
+++ models/rawrecord_import.go
@@ -40,13 +40,6 @@ func (config *DNSConfig) ImportRawRecords() error {
 			filePos := FixPosition(rawRec.FilePos)
 			typeName := rawRec.Type
 
-			//			if typeName == "IGNORE" {
-			//				continue
-			//			}
-			//			if typeName == "IMPORT_TRANSFORM" {
-			//				continue
-			//			}
-
 			// NB(tlim): We check if something is a builder first because LOC could be a builder or a dnsrdatav2.
 			if IsBuilder(typeName) {
 				records, err := dc.runBuilder(typeName, rawRec.TTL, rawRec.Args, rawRec.SubDomain)
