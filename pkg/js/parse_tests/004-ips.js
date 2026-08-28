3ddc83f6a7d03f9b05094840ee78be9c2991e076
Tom Limoncelli <tal@whatexit.org>
2026-08-28 11:21:02 -0400

    moretests

--- pkg/js/parse_tests/004-ips.js
+++ pkg/js/parse_tests/004-ips.js
@@ -1,12 +1,16 @@
-var REG = NewRegistrar("Third-Party", "NONE");
-var CF = NewDnsProvider("Cloudflare", "CLOUDFLAREAPI");
+var REG = NewRegistrar('Third-Party', 'NONE');
+var CF = NewDnsProvider('Cloudflare', 'CLOUDFLAREAPI');
 
-var BASE = IP("1.2.3.4");
-var HI = IP("190.2.3.4"); // An IP address with the highest bit set, to catch uint/int bugs.
+var BASE = IP('1.2.3.4');
 
-D("foo.com", REG, DnsProvider(CF, 0),
-    A("@", BASE),
-    A("p1", BASE + 1),
-    A("p255", BASE + 255),
-    A("hi", HI),
+D(
+    'foo.com',
+    REG,
+    DnsProvider(CF, 0),
+    A('@', BASE),
+    A('p1', BASE + 1),
+    A('p255', BASE + 255),
+
+    // An IP address with the highest bit set, to catch uint/int bugs.
+    A('yyy', IP('190.2.3.4'))
 );
