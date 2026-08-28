1468ebb3f8f098cb9a9f2c3dd4a9f16f3ec69ee0
Tom Limoncelli <tal@whatexit.org>
2026-08-28 11:13:52 -0400

    more tests

--- pkg/js/parse_tests/004-ips.js
+++ pkg/js/parse_tests/004-ips.js
@@ -2,9 +2,11 @@ var REG = NewRegistrar("Third-Party", "NONE");
 var CF = NewDnsProvider("Cloudflare", "CLOUDFLAREAPI");
 
 var BASE = IP("1.2.3.4");
+var HI = IP("190.2.3.4"); // An IP address with the highest bit set, to catch uint/int bugs.
 
 D("foo.com", REG, DnsProvider(CF, 0),
     A("@", BASE),
     A("p1", BASE + 1),
     A("p255", BASE + 255),
+    A("hi", HI),
 );
